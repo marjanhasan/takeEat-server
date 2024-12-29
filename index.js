e9555df577213ca2bb03f18b90ce9cc54b07d555
marjanhasan <moonmarjanhasan@gmail.com>
2024-12-29 21:10:13 +0600

    users create read delete update api added

--- index.js
+++ index.js
@@ -42,9 +42,36 @@ async function run() {
     });
 
     // user
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+
+      const query = { email: user?.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedRole = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(query, updatedRole);
       res.send(result);
     });
 
