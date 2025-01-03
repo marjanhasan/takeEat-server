979d13f18bd1dad4554d36ea1623b741021e24de
marjanhasan <moonmarjanhasan@gmail.com>
2025-01-03 19:45:54 +0600

    some revision done

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
 
     // verify admin
     const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded?.email;
+      const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === "admin";
